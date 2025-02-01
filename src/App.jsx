d7f955928aa0d5287f6c40919eacae133f9bf8e1
Chewsday <z647623@gmail.com>
2025-02-01 16:58:18 +0800

    week2

---
+++ src/App.jsx
@@ -0,0 +1,181 @@
+import { useRef, useState } from 'react';
+import axios from 'axios';
+
+import './App.scss';
+
+function App() {
+  const [tempProduct, setTempProduct] = useState(null);
+  
+  const [products, setProducts] = useState([]);
+
+
+
+  const userNameRaf = useRef(null);
+  const passWordRaf = useRef(null);
+
+  const [isAuth, setIsAuth] = useState(false);
+
+  function handleSubmit (e) {
+    e.preventDefault();
+
+
+    const account = {
+      username: userNameRaf.current.value,
+      password: passWordRaf.current.value
+    }
+
+    axios.post(`${import.meta.env.VITE_BASE_URL}/v2/admin/signin`, account)
+      .then(res => {
+        const { token, expired } = res.data;
+        document.cookie = `dogfood=${token}; expired=${new Date(expired)}`;
+        setIsAuth(true);
+
+        axios.defaults.headers.common['Authorization'] = token;
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_API_PATH}/admin/products`)
+          .then(res => {
+            setProducts(res.data.products)
+          }).catch(err => {
+            console.log(err);
+          })
+
+        console.log(res);
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
+
+  
+
+
+  return (
+    <>
+      {isAuth ? (
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-md-6">
+              <h2>產品列表</h2>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>產品名稱</th>
+                    <th>原價</th>
+                    <th>售價</th>
+                    <th>是否啟用</th>
+                    <th>查看細節</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products && products.length > 0 ? (
+                    products.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.title}</td>
+                        <td>{item.origin_price}</td>
+                        <td>{item.price}</td>
+                        <td>{item.is_enabled ? "啟用" : "未啟用"}</td>
+                        <td>
+                          <button
+                            className="btn btn-primary"
+                            onClick={() => setTempProduct(item)}
+                          >
+                            查看細節
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="5">尚無產品資料</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+            <div className="col-md-6">
+              <h2>單一產品細節</h2>
+              {tempProduct ? (
+                <div className="card mb-3">
+                  <img
+                    src={tempProduct.imageUrl}
+                    className="card-img-top primary-image"
+                    alt="主圖"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {tempProduct.title}
+                      <span className="badge bg-primary ms-2">
+                        {tempProduct.category}
+                      </span>
+                    </h5>
+                    <p className="card-text">
+                      商品描述：{tempProduct.description}
+                    </p>
+                    <p className="card-text">商品內容：{tempProduct.content}</p>
+                    <div className="d-flex">
+                      <p className="card-text text-secondary">
+                        <del>{tempProduct.origin_price}</del>
+                      </p>
+                      元 / {tempProduct.price} 元
+                    </div>
+                    <h5 className="mt-3">更多圖片：</h5>
+                    <div className="d-flex flex-wrap">
+                      {tempProduct.imagesUrl?.map((url, index) => (
+                        <img
+                          key={index}
+                          src={url}
+                          className="images"
+                          alt="副圖"
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <p className="text-secondary">請選擇一個商品查看</p>
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="container login">
+          <div className="row justify-content-center">
+            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
+            <div className="col-8">
+              <form id="form" className="form-signin" onSubmit={handleSubmit}>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="username"
+                    placeholder="name@example.com"
+                    ref={userNameRaf}
+                    required
+                    autoFocus
+                  />
+                  <label htmlFor="username">Email address</label>
+                </div>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Password"
+                    ref={passWordRaf}
+                    required
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+
+                <button className="btn btn-lg btn-primary w-100 mt-3">登入</button>
+              </form>
+            </div>
+          </div>
+          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App
